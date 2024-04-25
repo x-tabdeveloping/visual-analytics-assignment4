98a0e4156374917069d63d5433bb831601f6682b
Márton Kardos <power.up1163@gmail.com>
2024-04-25 17:39:27 +0200

    Added main function to aggregate file

--- src/aggregate_results.py
+++ src/aggregate_results.py
@@ -9,22 +9,29 @@ def percent_larger_than_zero(s: pd.Series) -> int:
     return 100 * (np.sum(s > 0) / len(s))
 
 
-results_dir = Path("results")
-figures_dir = Path("figures")
-figures_dir.mkdir(exist_ok=True)
+def main():
+    results_dir = Path("results")
+    figures_dir = Path("figures")
+    figures_dir.mkdir(exist_ok=True)
 
-faces = pd.read_json(results_dir.joinpath("faces.jsonl"), orient="records", lines=True)
-faces = faces[faces["type"] == "success"]
-faces["n_faces"] = faces["n_faces"].astype(int)
+    faces = pd.read_json(
+        results_dir.joinpath("faces.jsonl"), orient="records", lines=True
+    )
+    faces = faces[faces["type"] == "success"]
+    faces["n_faces"] = faces["n_faces"].astype(int)
 
-aggregated = faces.groupby(["journal", "decade"]).agg(
-    total_faces=("n_faces", "sum"),
-    percent_contains_face=("n_faces", percent_larger_than_zero),
-)
-for journal, data in aggregated.groupby("journal"):
-    fig = px.line(data.reset_index(), x="decade", y="percent_contains_face")
-    fig.update_layout(width=800, height=800)
-    fig.update_layout(xaxis_title="Decade")
-    fig.update_layout(yaxis_title="% pages with faces")
-    fig.write_image(figures_dir.joinpath(f"{journal}.png"), scale=2)
-    data.to_csv(results_dir.joinpath(f"{journal}.csv"))
+    aggregated = faces.groupby(["journal", "decade"]).agg(
+        total_faces=("n_faces", "sum"),
+        percent_contains_face=("n_faces", percent_larger_than_zero),
+    )
+    for journal, data in aggregated.groupby("journal"):
+        fig = px.line(data.reset_index(), x="decade", y="percent_contains_face")
+        fig.update_layout(width=800, height=800)
+        fig.update_layout(xaxis_title="Decade")
+        fig.update_layout(yaxis_title="% pages with faces")
+        fig.write_image(figures_dir.joinpath(f"{journal}.png"), scale=2)
+        data.to_csv(results_dir.joinpath(f"{journal}.csv"))
+
+
+if __name__ == "__main__":
+    main()
