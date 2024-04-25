31dfbbd16ccb6ba06df716eff47fb6e06c52cfe5
Márton Kardos <power.up1163@gmail.com>
2024-04-25 17:26:10 +0200

    Added plotting and saving to aggregation script

--- src/aggregate_results.py
+++ src/aggregate_results.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
+import plotly.express as px
 
 
 def percent_larger_than_zero(s: pd.Series) -> int:
     return 100 * (np.sum(s > 0) / len(s))
 
 
-faces = pd.read_json("results/faces.jsonl", orient="records", lines=True)
+results_dir = Path("results")
+figures_dir = Path("figures")
+figures_dir.mkdir(exist_ok=True)
+
+faces = pd.read_json(results_dir.joinpath("faces.jsonl"), orient="records", lines=True)
 faces = faces[faces["type"] == "success"]
 faces["n_faces"] = faces["n_faces"].astype(int)
 
@@ -14,5 +21,10 @@ aggregated = faces.groupby(["journal", "decade"]).agg(
     total_faces=("n_faces", "sum"),
     percent_contains_face=("n_faces", percent_larger_than_zero),
 )
-
-aggregated
+for journal, data in aggregated.groupby("journal"):
+    fig = px.line(data.reset_index(), x="decade", y="percent_contains_face")
+    fig.update_layout(width=800, height=800)
+    fig.update_layout(xaxis_title="Decade")
+    fig.update_layout(yaxis_title="% pages with faces")
+    fig.write_image(figures_dir.joinpath(f"{journal}.png"), scale=2)
+    data.to_csv(results_dir.joinpath(f"{journal}.csv"))
